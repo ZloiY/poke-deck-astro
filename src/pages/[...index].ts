77b34bba533899391681656309e1fca3ebb0a206
Maksim Bakhanovich <maksim.bakhanovich@protonmail.com>
2023-03-21 12:12:00 +0100

    fix(timeout): trying to fix timout

--- src/pages/[...index].ts
+++ src/pages/[...index].ts
@@ -2,17 +2,16 @@ import type { APIRoute } from "astro"
 
 
 export const get: APIRoute = async ({ params, redirect }) => {
-    console.log('params', params)
   return redirect('home', 307)
 };
 
 export const all: APIRoute = async () => {
-    const headers = new Headers({
-        Allow: 'OPTIONS, GET, POST',
-    })
-    console.log('called')
-    return new Response(null, {
-        status: 200,
-        headers,
-    })
+  const headers = new Headers({
+      Allow: 'OPTIONS, GET, POST',
+  })
+  console.log('called')
+  return new Response(null, {
+      status: 200,
+      headers,
+  })
 }
