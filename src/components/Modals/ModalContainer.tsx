764ec203be708318a5ad36533d4b0dc31ecacf32
Maksim Bakhanovich <maksim.bakhanovich@protonmail.com>
2023-03-22 13:59:12 +0100

    refactor(modals): reduce bundle size

--- src/components/Modals/ModalContainer.tsx
+++ src/components/Modals/ModalContainer.tsx
@@ -1,5 +1,5 @@
 import type { Store } from "effector";
-import { AnimatePresence, motion, useSpring } from "framer-motion";
+import { AnimatePresence, LazyMotion, domAnimation, m } from "framer-motion";
 import type { ReactNode } from "react";
 import ReactModal from "react-modal";
 import { useModalState } from "src/hooks";
@@ -37,30 +37,32 @@ export const ModalContainer = ({
       className="static outline-none"
       onRequestClose={onRequestClose}
     >
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 1 }}
-          transition={{ duration: 0.15, type: "spring", stiffness: 100 }}
-          className={twMerge(
-            title &&
-              "bg-purple-900 text-white rounded-xl flex flex-col relative shadow-[0_0_20px_5px] shadow-purple-500",
-          )}
-        >
-          {title && (
-            <div className="flex justify-between mb-2 p-3 border-b-2 border-yellow-500">
-              <span className="text-2xl font-coiny">{title}</span>
-              <Close
-                role="button"
-                className="cursor-pointer w-8 h-8 hover:text-yellow-400"
-                onClick={onRequestClose}
-              />
-            </div>
-          )}
-          {children(onRequestClose)}
-        </motion.div>
-      </AnimatePresence>
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence>
+          <m.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 1 }}
+            transition={{ duration: 0.15, type: "spring", stiffness: 100 }}
+            className={twMerge(
+              title &&
+                "bg-purple-900 text-white rounded-xl flex flex-col relative shadow-[0_0_20px_5px] shadow-purple-500",
+            )}
+          >
+            {title && (
+              <div className="flex justify-between mb-2 p-3 border-b-2 border-yellow-500">
+                <span className="text-2xl font-coiny">{title}</span>
+                <Close
+                  role="button"
+                  className="cursor-pointer w-8 h-8 hover:text-yellow-400"
+                  onClick={onRequestClose}
+                />
+              </div>
+            )}
+            {children(onRequestClose)}
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </ReactModal>
   );
 };
